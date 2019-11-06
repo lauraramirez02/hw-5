5046678d37dcb200b38d3d5cf21572adad2cb2e6
Laura Ramirez <laura.ramirez02@student.csulb.edu>
2019-11-06 13:03:08 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-var xhr = new XMLHttpRequest(); 
+'use strict'
+let array = []
 
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        if (this.status == 200) {
-            var result = JSON.parse(this.responseText);
-            users(result);
+// XMLHttpsRequest 
+let alpha = function(arr){
+    let newArr = [];
+  for (let i=0; i < arr.length; ++i){
+    newArr.push(arr[i].email);
+  }
+  return (newArr.sort());
+}
+let div = function(arr){
+    arr = alpha(arr);
+    let divEl = document.getElementById('emails');
+    for (let i=0; i < arr.length; i++){
+        let child = document.createElement('p');
+        let text = document.createTextNode(arr[i]);
+        child.append(text);
+        divEl.appendChild(child);
+    }
+};
+
+let httpRequest = function(){
+    const req = new XMLHttpRequest();
+    req.open('GET','https://jsonplaceholder.typicode.com/users');
+    req.onload = function(){
+        if (req.status == 200){
+            console.log('Status:',req.status);
+            array = JSON.parse(req.response);
+            div(array);
+        }else{
+            console.log('ERROR',req.statusText);
         }
-    }   
-}
-xhr.open("get", 'https://jsonplaceholder.typicode.com/users', true);
-    
-xhr.send();
-
-function users(arr) {
-    var emails = [];
-    var display = '';
-    var i;
-    for(i=0; i<arr.length; i++) {
-        emails.push(arr[i].email);
     }
-    emails.sort();
-    for (i=0; i<emails.length; i++) {
-        display += '"'+ emails[i] + '",';
+    req.onerror = function(){
+            console.log('Network Error');
     }
-    document.getElementById('output').innerHTML = display;
+    req.send()
 }
+httpRequest();
 
+// Fetch 
+const byLength = function (x,y){
+  if(x.username.length < y.username.length){
+    return -1
+  }else {
+    return 1
+  }
+}
+const writeToFetch = function(data){
+  let div2 = document.getElementById('users');
+  data = data.sort(byLength);
+  for (let i=0; i < data.length; ++i){
+    let child = document.createElement('p');
+    let text = document.createTextNode(data[i].username);
+    child.append(text);
+    div2.append(child);
+  }
+}
 fetch('https://jsonplaceholder.typicode.com/users')
-    .then (response => response.json())
-    .then (result2 => {
-        const users = result2.sort ((a,b) => a.username.length -b.username.length);
-        var sortUsers = [];
-        var parsedUsers = JSON.parse(JSON.stringify(users))
-  
-        for (i=0; i< parsedUsers.length; i++) {
-            sortUsers += '"'+ parsedUsers[i].username + '",';
-        }
-
-document.getElementById("output2").innerHTML = sortUsers
-    })
+  .then(function(response){
+    return response.json()
+  }).then(function(data){
+      writeToFetch(data);
+  })
